7adc72ecc27982f8b076d095632176ad046c2032
unknown <sh.rusell@gmail.com>
2023-10-17 18:59:46 +0500

    add:technology info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.html';
 import './style.scss';
 import './script/script.js';
+import './script/technology.js';
---
+++ src/script/technology.js
@@ -0,0 +1,5 @@
+const face = document.querySelector('.technology__face');
+
+document.querySelector('.face__point-top').addEventListener('click',()=>{
+  face.classList.add('click-top')
+})
